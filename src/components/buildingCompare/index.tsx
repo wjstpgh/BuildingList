c1bc3778fdff994823c49ce5aa3dc993bb23cd2f
wjstpgh <sehoJ3756@gmail.com>
2024-05-08 01:36:50 +0900

    5-compare-building 빌딩 비교 api 표현 ui 제작

--- src/components/buildingCompare/index.tsx
+++ src/components/buildingCompare/index.tsx
@@ -1,88 +1,19 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Button from "../common/button";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { buildingList } from "../../store";
 import axios from "axios";
 import { DetailBuildingData } from "../buildingList/data";
-
-type CompareBuildingList = {
-  buildingName: {
-    view: string;
-    data: string[];
-  };
-  image: {
-    view: string;
-    data: string[];
-  };
-  construct: {
-    view: string;
-    data: {
-      year: number;
-      quarter: string;
-    }[];
-  };
-  deposit: {
-    view: "보증금";
-    data: number[];
-  };
-  rentFee: {
-    view: "임대료";
-    data: number[];
-  };
-  maintenanceFee: {
-    view: "관리비";
-    data: number[];
-  };
-  transactionDate: {
-    view: "최근거래일";
-    data: {
-      year: number;
-      month: number;
-    }[];
-  };
-  transactionPrice: {
-    view: "거래가";
-    data: number[];
-  };
-};
-
-const compareBuildingList: CompareBuildingList = {
-  buildingName: {
-    view: "비교빌딩",
-    data: [],
-  },
-  image: {
-    view: "건물 사진",
-    data: [],
-  },
-  construct: {
-    view: "준공연도",
-    data: [],
-  },
-  deposit: {
-    view: "보증금",
-    data: [],
-  },
-  rentFee: {
-    view: "임대료",
-    data: [],
-  },
-  maintenanceFee: {
-    view: "관리비",
-    data: [],
-  },
-  transactionDate: {
-    view: "최근거래일",
-    data: [],
-  },
-  transactionPrice: {
-    view: "거래가",
-    data: [],
-  },
-};
+import Loading from "../common/loading";
+import { parseNum } from "../../utils";
+import { defaultCompareBuildingList } from "./data";
 
 const BuildingCompare = () => {
+  const [compareBuildingList, setCompareBuildingList] = useState(
+    defaultCompareBuildingList
+  );
+  const [isLoading, setIsLoading] = useState(false);
   const compareBuildingKeyList = useRecoilValue(buildingList);
   const push = useNavigate();
 
@@ -92,17 +23,22 @@ const BuildingCompare = () => {
 
   const hydrateCompareList = useCallback(async () => {
     compareBuildingKeyList.forEach(async (key) => {
+      setIsLoading(true);
       const {
         data: { data: buildingData },
       } = await axios.get<DetailBuildingData>(`building/${key}`);
-      Object.keys(compareBuildingList).forEach((category) =>
-        compareBuildingList[
-          category as keyof typeof compareBuildingList
-        ].data.concat(
-          buildingData[category as keyof typeof compareBuildingList]
-        )
-      );
-      console.log(buildingData);
+
+      setCompareBuildingList((prev) => {
+        const newList = prev;
+        Object.keys(newList).forEach((category) =>
+          newList[category as keyof typeof prev].data.push(
+            buildingData[category as keyof typeof prev] as never
+          )
+        );
+
+        return newList;
+      });
+      setIsLoading(false);
     });
   }, [compareBuildingKeyList]);
 
@@ -119,7 +55,67 @@ const BuildingCompare = () => {
         classBtnWrap="self-end"
         btnStyleType="curtain"
       />
-      <div></div>
+      <div className="w-[90vw] flex flex-col gap-y-[10px] overflow-x-scroll">
+        {!isLoading ? (
+          Object.entries(compareBuildingList).map(([category, value]) => (
+            <div
+              key={category}
+              className="flex gap-x-[20px] [&>div]:w-[200px] [&>div]:shrink-0 [&>div]:flex [&>div]:items-center"
+            >
+              <div>{value.view}</div>
+              {value.data.map((data, idx) =>
+                category === "buildingName" ? (
+                  <div key={`${category}${idx}`}>{data as string}</div>
+                ) : category === "image" ? (
+                  <div key={`${category}${idx}`}>
+                    <img
+                      src={data as string}
+                      alt="compare img"
+                      width={200}
+                      height={200}
+                      className="w-full h-full"
+                    />
+                  </div>
+                ) : category === "construct" ? (
+                  <div key={`${category}${idx}`}>{`${
+                    (data as { year: number; quarter: string }).year
+                  }(${
+                    (data as { year: number; quarter: string }).quarter
+                  })`}</div>
+                ) : category === "transactionDate" ? (
+                  <div key={`${category}${idx}`}>{`${
+                    (data as { year: number; month: number }).year
+                  }.${(data as { year: number; month: number }).month}`}</div>
+                ) : category === "transactionPrice" ? (
+                  <div
+                    key={`${category}${idx}`}
+                    className={`${
+                      Math.min(...(value.data as number[])) === data &&
+                      "text-[#F49898]"
+                    } ${
+                      Math.max(...(value.data as number[])) === data &&
+                      "text-[#8870ED]"
+                    }`}
+                  >{`${parseNum(data as number)}원`}</div>
+                ) : (
+                  <div
+                    key={`${category}${idx}`}
+                    className={`${
+                      Math.min(...(value.data as number[])) === data &&
+                      "text-[#F49898]"
+                    } ${
+                      Math.max(...(value.data as number[])) === data &&
+                      "text-[#8870ED]"
+                    }`}
+                  >{`${parseNum(data as number)}원/평`}</div>
+                )
+              )}
+            </div>
+          ))
+        ) : (
+          <Loading />
+        )}
+      </div>
     </div>
   );
 };
